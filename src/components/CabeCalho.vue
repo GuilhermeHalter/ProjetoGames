6ca73ad5280a71b2a3a98f785e59a4b24ffb9a1d
guilhermehalter <guisam.halter@gamil.com>
2022-05-16 10:59:49 -0300

    alterações

--- src/components/CabeCalho.vue
+++ src/components/CabeCalho.vue
@@ -3,6 +3,7 @@ export default {};
 </script>
 <template>
   <header id="header">
+    <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <img class="logo" src="@/assets/LogoSite.png" alt="">
     <div class="opition">
       <a href="">Snes</a>
@@ -10,6 +11,15 @@ export default {};
       <a href="">Mega Drive</a>
       <a href="">Sega Cd</a>
       <a href="">Atari</a>
+
+      <div class="barra">
+      <input type="text" class="form-control new-task">
+      <button >
+      <span class="material-symbols-outlined">search</span>
+      </button>
+
+      </div>
+
     </div>
   </header>
 </template>
